eb918bdbdb2fc2facd2be2f9d71ff04440e1e415
SpriderManzz <741149026@qq.com>
2019-01-17 17:43:17 +0800

    111

--- framework/browser_engine.py
+++ framework/browser_engine.py
@@ -10,7 +10,7 @@ class BrowserEngine(object):
        os.path.dirname(os.path.abspath('.')) 获取的是E:\UI003
     """
     def __init__(self, driver):
-        self.driver = driver
+        self.driver = driver   # 定位为属性难道是为了让类中的所有方法都能使用？
 
     # read the browser type from config.ini file, return the driver
     def open_browser(self):
@@ -26,7 +26,7 @@ class BrowserEngine(object):
         :return: driver
         """
         if browser_type == 'Firefox':
-            driver = webdriver.Firefox()
+            driver = webdriver.Firefox()   # driver是一个浏览器实例
         elif browser_type == 'Chrome':
             driver = webdriver.Chrome()
         elif browser_type == 'IE':
@@ -35,7 +35,9 @@ class BrowserEngine(object):
             driver = webdriver.Firefox()
         driver.get(url)
 
-        return driver
+
+        return driver  # 返回根据配置文件选择的浏览器实例
 
     def quit_browser(self):
         self.driver.quit()
+
--- test/test_baidu_homepage2.py
+++ test/test_baidu_homepage2.py
@@ -10,26 +10,31 @@ class BadiduHomePageTaseCse(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         browser = BrowserEngine(cls)
+        browser.open_browser()
 
-    def test_baidu_search(self):
+    # def test_baidu_search(self):
+    #
+    #     self.baiduhomepage.type_search('selenium')  # 调用页面对象中的方法,传了一个参数，然后
+    #     # type_search再调用type方法，传了2个参数input_box元素的定位和元素的值，type再调find_element方法传递元素的定位
+    #     self.baiduhomepage.send_submit_btn()  # 调用页面对象类中的点击搜索按钮方法
+    #
+    #     try:
+    #         assert 'selenium' in self.baiduhomepage.get_page_title()  # 调用页面对象继承基类中的获取页面标题方法
+    #         print('Test Pass.')
+    #     except Exception as e:
+    #         print('Test Fail.', format(e))
+    #
+    # def test_title(self):
+    #     """验证搜索标题是否正确"""
+    #     try:
+    #         assert u'selenium_百度搜索' in self.baiduhomepage.get_page_title()
+    #         print('Test Pass.')
+    #     except Exception as e:
+    #         print('Test Fail.', format(e))
 
-        self.baiduhomepage.type_search('selenium')  # 调用页面对象中的方法,传了一个参数，然后
-        # type_search再调用type方法，传了2个参数input_box元素的定位和元素的值，type再调find_element方法传递元素的定位
-        self.baiduhomepage.send_submit_btn()  # 调用页面对象类中的点击搜索按钮方法
+    def test_print(self):
+        print ("测试test_print")
 
-        try:
-            assert 'selenium' in self.baiduhomepage.get_page_title()  # 调用页面对象继承基类中的获取页面标题方法
-            print('Test Pass.')
-        except Exception as e:
-            print('Test Fail.', format(e))
-
-    def test_title(self):
-        """验证搜索标题是否正确"""
-        try:
-            assert u'selenium_百度搜索' in self.baiduhomepage.get_page_title()
-            print('Test Pass.')
-        except Exception as e:
-            print('Test Fail.', format(e))
 
     @classmethod
     def tearDownClass(cls):
